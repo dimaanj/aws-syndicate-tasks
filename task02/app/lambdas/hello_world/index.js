e3c79bd8a13e88d9b02c7ba088f4a31ac38236bf
Dzmitry Tamashevich <diaamnj@mail.ru>
2024-05-04 09:19:51 +0300

    remove checker

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -3,12 +3,12 @@ exports.handler = async (event) => {
     console.log("Request method:", event.requestContext.http.method);
     console.log("Request path: ", event.rawPath);
 
-    if(event.rawPath !== '/hello') {
-        return {
-            statusCode: 400,
-            message: `Bad request syntax or unsupported method. Request path: ${event.rawPath}. HTTP method: ${event.requestContext.http.method}`
-        }
-    }
+    // if(event.rawPath !== '/hello') {
+    //     return {
+    //         statusCode: 400,
+    //         message: `Bad request syntax or unsupported method. Request path: ${event.rawPath}. HTTP method: ${event.requestContext.http.method}`
+    //     }
+    // }
     
     const response = {
         statusCode: 200,
